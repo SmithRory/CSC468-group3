efdab0fa7fbd907f2b56c591132fb01598f8bdc8
Oliver Lewis <oliverlewis1339@gmail.com>
2021-01-25 11:10:51 -0800

    Basic command handling layout

--- backend/src/cmd_handler.py
+++ backend/src/cmd_handler.py
@@ -1,47 +1,80 @@
 
 def handle_command(cmd, params):
     
-    switcher = {
-
+    switch = {
+        "ADD": add,
+        "QUOTE": quote,
+        "COMMIT_BUY": commit_buy,
+        "CANCEL_BUY": cancel_buy,
+        "SELL": sell,
+        "COMMIT_SELL": commit_sell,
+        "CANCEL_SELL": cancel_sell,
+        "SET_BUY_AMOUNT": set_buy_amount, 
+        "CANCEL_SET_BUY": cancel_set_buy,
+        "SET_BUY_TRIGGER": set_buy_trigger,
+        "SET_SELL_AMOUNT": set_sell_amount,
+        "SET_SELL_TRIGGER": set_sell_trigger,
+        "CANCEL_SET_SELL": cancel_set_sell,
+        "DUMPLOG": dumplog,
+        "DISPLAY_SUMMARY": display_summary
     }
     
-    if cmd == "ADD":
-        if len(params) != 2:
+    # Get the function
+    func = switch.get(cmd, unknown_cmd)
+    # Call the function to handle the command
+    func(params)
 
-def add():
+def add(params):
+    print("ADD: ", params)
+    
+def quote(params):
+    print("QUOTE: ", params)
 
-def quote():
+def commit_buy(params):
+    print("COMMIT_BUY: ", params)
 
-def commit_buy():
+def cancel_buy(params):
+    print("CANCEL_BUY: ", params)
 
-def cancel_buy():
+def sell(params):
+    print("SELL: ", params)
 
-def sell():
+def commit_sell(params):
+    print("COMMIT_SELL: ", params)
 
-def commit_sell():
+def cancel_sell(params):
+    print("CANCEL_SELL: ", params)
 
-def cancel_sell():
+def set_buy_amount(params):
+    print("SET_BUY_AMOUNT: ", params)
 
-def set_buy_amount():
+def cancel_set_buy(params):
+    print("CANCEL_SET_BUY: ", params)
 
-def cancel_set_buy():
+def set_buy_trigger(params):
+    print("SET_BUY_TRIGGER: ", params)
 
-def set_buy_trigger():
+def set_sell_amount(params):
+    print("SET_SELL_AMOUNT: ", params)
 
-def set_sell_amount():
+def set_sell_trigger(params):
+    print("SET_SELL_TRIGGER: ", params)
 
-def set_sell_trigger():
+def cancel_set_sell(params):
+    print("CANCEL_SET_SELL: ", params)
 
-def cancle_set_sell():
+def dumplog(params):
+    print("DUMPLOG: ", params)
 
-def dumplog():
+def display_summary(params):
+    print("DISPLAY_SUMMARY: ", params)
 
-def display_summary():
-
-
+def unknown_cmd(params):
+    print("UNKNOWN COMMAND!")
     
 # TODO
 if __name__ == "__main__":
-    print('In main')
-    handle_command()
+    command = "SELL"
+    parameters = ("oY01WVirLr", "S", 641.90)
+    handle_command(command, parameters)
 
