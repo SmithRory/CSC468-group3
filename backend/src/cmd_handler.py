37093310c5ac13b7962fab57e7399ed9b65c8ecb
Oliver Lewis <oliverlewis1339@gmail.com>
2021-01-25 10:25:20 -0800

    added basic layout for command handling

---
+++ backend/src/cmd_handler.py
@@ -0,0 +1,47 @@
+
+def handle_command(cmd, params):
+    
+    switcher = {
+
+    }
+    
+    if cmd == "ADD":
+        if len(params) != 2:
+
+def add():
+
+def quote():
+
+def commit_buy():
+
+def cancel_buy():
+
+def sell():
+
+def commit_sell():
+
+def cancel_sell():
+
+def set_buy_amount():
+
+def cancel_set_buy():
+
+def set_buy_trigger():
+
+def set_sell_amount():
+
+def set_sell_trigger():
+
+def cancle_set_sell():
+
+def dumplog():
+
+def display_summary():
+
+
+    
+# TODO
+if __name__ == "__main__":
+    print('In main')
+    handle_command()
+
