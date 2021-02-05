e2ca2af1cfece7f974ced0155ecc3f1460f98c73
Oliver Lewis <oliverlewis1339@gmail.com>
2021-02-05 15:37:26 -0800

    Updated to use mongoengine instead of pymongo

--- worker/src/database/connect.py
+++ worker/src/database/connect.py
@@ -1,8 +1,10 @@
 import os
-from pymongo import MongoClient
+#from pymongo import MongoClient
+from mongoengine import connect
 
+# This currently isn't used. Should be able to eventually be deleted.
 class Connect(object):
     @staticmethod
     def get_connection():
         mongo_uri = 'mongodb://' + os.environ['MONGODB_USERNAME'] + ':' + os.environ['MONGODB_PASSWORD'] + '@' + os.environ['MONGODB_HOSTNAME'] + ':27017/' + os.environ['MONGODB_DATABASE']
-        return MongoClient(mongo_uri)
+        connect(host = mongo_uri)
