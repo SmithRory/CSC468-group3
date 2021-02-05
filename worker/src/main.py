8aca1ef860da4526549c49112a73845f3a171680
Rory Smith <rory.smith2475@gmail.com>
2021-02-05 14:39:13 -0800

    Updated rabbit consumer to work with ioloop and handle setup without crashing

--- worker/src/main.py
+++ worker/src/main.py
@@ -4,7 +4,11 @@ import os
 import pika
 import queue
 import time
-from threading import Thread, Lock
+from rabbitmq.consumer import Consumer
+from threading import Thread
+
+print("Start of file")
+sys.stdout.flush()
 
 # Handles exiting when SIGTERM (sent by ^C input) is received 
 # in a gracefull way. Main loop will only exit after a completed iteration
@@ -15,37 +19,26 @@ def exit_gracefully(self, signum, frame):
 signal.signal(signal.SIGINT, exit_gracefully)
 signal.signal(signal.SIGTERM, exit_gracefully)
 
-mutex = Lock()
-
-messages = []
-
-def queue_callback(ch, method, properties, body) -> None:
-    ch.basic_ack(delivery_tag=method.delivery_tag)
-
-    mutex.acquire()
-    messages.append(body.decode())
-    print(f'Received: {body.decode()}')
-    sys.stdout.flush()
-    mutex.release()
-
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='rabbitmq'))
-channel = connection.channel()
-
-channel.exchange_declare(exchange='backend')
-channel.queue_declare(queue='backend_queue')
-channel.queue_bind(exchange='backend', queue='backend_queue')
-
-channel.basic_consume(
-    queue='backend_queue',
-    on_message_callback=queue_callback
+message_queue = queue.Queue()
+rabbit_queue = Consumer(
+    command_queue=message_queue,
+    connection_param='rabbitmq',
+    exchange_name='backend',
+    queue_name='backend_queue',
+    routing_key='backend_queue'
 )
 
 if __name__ == "__main__":
-    t_consumer = Thread(target=channel.start_consuming)
+    t_consumer = Thread(target=rabbit_queue.run)
     t_consumer.start()
 
+    print("Started consumer thread")
+    sys.stdout.flush()
+
     while not EXIT_PROGRAM:
+        if not message_queue.empty():
+            print(f"Message: {message_queue.get()}")
+            sys.stdout.flush()
         time.sleep(0.1)
 
     t_consumer.join()
