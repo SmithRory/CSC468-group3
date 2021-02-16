9f9fe8b4818da099549dc3b28140a81f462f40af
Rory Smith <rory.smith2475@gmail.com>
2021-02-15 16:05:57 -0800

    Fixed Dockerfile so that it actually works

--- manager/src/main.rs
+++ manager/src/main.rs
@@ -1,6 +1,18 @@
 use std::{thread, time};
 
+extern crate bollard;
+extern crate lapin;
+
+use bollard::Docker;
+use lapin::{
+    options::*, publisher_confirm::Confirmation, types::FieldTable, BasicProperties, Connection,
+    ConnectionProperties, Result,
+};
+
 fn main() {
+    #[cfg(unix)]
+    Docker::connect_with_unix_defaults();
+
     loop {
         println!("Congratulations your application is successfully containerized with Docker!!");
         let one_second = time::Duration::from_secs(1);
