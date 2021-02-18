576edc36aeca056f6b83ed7589cdb3f40652e77f
janhavidudhat <janhavidudhat@uvic.ca>
2021-02-17 22:55:13 -0800

    fix

--- worker/src/legacy/quote.py
+++ worker/src/legacy/quote.py
@@ -49,7 +49,7 @@ def get_quote(uid : str, stock_name : str, transactionNum : int, userCommand : s
             })
 
             # update after trying on quote server, update quote server time too
-            QuoteServerType().log(timestamp=round(timestampForLog*1000), transactionNum=transactionNum, price=response[0], stockSymbol=stock_name, username=uid, quoteServerTime=response[3], cryptokey=response[4])
+            QuoteServerType().log(transactionNum=transactionNum, price=response[0], stockSymbol=stock_name, username=uid, quoteServerTime=response[3], cryptokey=response[4])
 
             return response[0] # Only returns the stock price
 
@@ -63,6 +63,6 @@ def get_quote(uid : str, stock_name : str, transactionNum : int, userCommand : s
     # add user funds after confirming
     # System Event log since received from cache
 
-    SystemEventType().log(timestamp=timestampForLog, transactionNum=transactionNum, command=userCommand, username=uid, stockSymbol=stock_name)
+    SystemEventType().log(transactionNum=transactionNum, command=userCommand, username=uid, stockSymbol=stock_name)
 
     return result.value
