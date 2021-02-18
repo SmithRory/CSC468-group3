75877b04cd0cf751b04e01dc3775cfad86b38e47
Rory Smith <rory.smith2475@gmail.com>
2021-02-17 20:58:20 -0800

    Fixed quote server command

--- worker/src/legacy/quote.py
+++ worker/src/legacy/quote.py
@@ -12,9 +12,9 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 def quote_server_connect() -> bool:
     global s
-    print(type(s))
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.settimeout(4)
         s.connect((QUOTE_ADDRESS, PORT))
         s.settimeout(None)
@@ -30,12 +30,12 @@ def get_quote(uid : str, stock_name : str, transactionNum : int, userCommand : s
     timestampForLog = round(time.time()*1000)
     
     if result is None or time.time() - result.timestamp >= quote_cache.UPDATE_FREQ:
-        command = f'QUOTE,{uid},{stock_name}\n'
+        command = f'{stock_name}, {uid}\n'
 
         try:
-            s.send(command.encode())
+            s.send(command.encode('utf-8'))
             data = s.recv(1024)
-            response = parser.quote_result_parse(data.decode())
+            response = parser.quote_result_parse(data.decode('utf-8'))
             if len(response) < 2 :
                 print(f"Not connected to quote server...")
                 quote_server_connect()
