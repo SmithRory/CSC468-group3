9300ff2ce25ef090e952b5d9e42364269437078e
janhavidudhat <janhavidudhat@uvic.ca>
2021-02-21 22:03:18 -0800

    test, do not pull

--- worker/src/legacy/quote.py
+++ worker/src/legacy/quote.py
@@ -41,14 +41,15 @@ def get_quote(uid : str, stock_name : str, transactionNum : int, userCommand : s
             return response[0] # Only returns the stock price
 
         except socket.error:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            try:
-                quote_server_connect()
-            except socket.timeout:
-                print("Socket connection timeout")
-                time.sleep(0.1) # Just to reduce spam error messages
-
-            return get_quote(uid, stock_name, transactionNum, userCommand)
+            return 123.23
+#             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#             try:
+#                 quote_server_connect()
+#             except socket.timeout:
+#                 print("Socket connection timeout")
+#                 time.sleep(0.1) # Just to reduce spam error messages
+#
+#             return get_quote(uid, stock_name, transactionNum, userCommand)
 
 
 
