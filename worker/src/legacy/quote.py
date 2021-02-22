9732532d57379548fc64339dc8cbbdbcf368f78d
janhavidudhat <janhavidudhat@uvic.ca>
2021-02-21 20:34:47 -0800

    Replace caching with Redis as cache

--- worker/src/legacy/quote.py
+++ worker/src/legacy/quote.py
@@ -20,10 +20,10 @@ def quote_server_connect():
 def get_quote(uid : str, stock_name : str, transactionNum : int, userCommand : str) -> float:
     global s
 
-    result = quote_cache.cache.get(stock_name, None)
+    result = quote_cache.get(stock_name)
     timestampForLog = round(time.time()*1000)
     
-    if result is None or time.time() - result.timestamp >= quote_cache.UPDATE_FREQ:
+    if result is None:
         command = f'{stock_name}, {uid}\n'
 
         try:
@@ -37,14 +37,7 @@ def get_quote(uid : str, stock_name : str, transactionNum : int, userCommand : s
 
             timestampForLog = time.time()
 
-            quote_cache.cache.update({
-                stock_name: quote_cache.Quote
-                (
-                    stock_name=stock_name,
-                    value=response[0],
-                    timestamp=timestampForLog
-                )
-            })
+            quote_cache.add(stock_name, response[0], response[3])
 
             # update after trying on quote server, update quote server time too
             QuoteServerType().log(transactionNum=transactionNum, price=response[0], stockSymbol=stock_name, username=uid, quoteServerTime=response[3], cryptokey=response[4])
@@ -65,7 +58,7 @@ def get_quote(uid : str, stock_name : str, transactionNum : int, userCommand : s
 
     # add user funds after confirming
     # System Event log since received from cache
-
+    print("Quote used from cache!!")
     SystemEventType().log(transactionNum=transactionNum, command=userCommand, username=uid, stockSymbol=stock_name)
 
     return result.value
--- worker/src/legacy/quote_cache.py
+++ worker/src/legacy/quote_cache.py
@@ -1,16 +1,33 @@
-from dataclasses import dataclass
-import time
+import redis
+from datetime import timedelta
+
+# from dataclasses import dataclass
+# import time
 
 ''' This is a temporary replacement for Reddis cache
 for use in the 1 user workload test only. Going forward
 Reddis will be implemented and replace this file.
 '''
 
-@dataclass
-class Quote:
-    stock_name: str
-    value: float
-    timestamp: float
+# @dataclass
+# class Quote:
+#     stock_name: str
+#     value: float
+#     timestamp: float
+#
+# cache = {} # {stock_name: Quote}
+# UPDATE_FREQ = 4 # Values are outdated after 4 seconds
 
-cache = {} # {stock_name: Quote}
-UPDATE_FREQ = 4 # Values are outdated after 4 seconds
+
+def add(stock_symbol, stock_price, quoteServerTime):
+    r = redis.Redis(host='redishost')
+    r.sadd(stock_symbol, stock_price)
+    r.expire(stock_symbol, (quoteServerTime+1000))
+
+# param:
+#       stock_symbol : String value of upto three letters For e.g., ABC
+# returns:
+#       stock_price : value of the stock
+def get(stock_symbol):
+    r = redis.Redis(host='redishost')
+    return r.get(stock_symbol)
