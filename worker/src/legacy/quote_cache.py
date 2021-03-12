7ef3eb45366f3bca61d617d346bd7904306df3f2
janhavidudhat <janhavidudhat@uvic.ca>
2021-03-12 10:02:38 -0800

    bug fix - cache - typo in argument

--- worker/src/legacy/quote_cache.py
+++ worker/src/legacy/quote_cache.py
@@ -1,18 +1,21 @@
-# import redis
-# from datetime import timedelta
-
-
+# param:
+#       stock_symbol : String value of upto three letters For e.g., ABC
+#       stock_price : The value of the stock
+#       quoteServerTime : The time when the stock_price was received, this is for calculating the expiry time
+#       redisHost : The redis host the worker is connected to
+# returns:
+#       stock_price : value of the stock
+#
 def add(stock_symbol, stock_price, quoteServerTime, redisHost):
-#     r = redis.Redis(host='redishost')
     redisHost.set(stock_symbol, stock_price)
     redisHost.expire(stock_symbol, (int(quoteServerTime)+1000))
 
 # param:
 #       stock_symbol : String value of upto three letters For e.g., ABC
+#       redisHost : The redis host the worker is connected to
 # returns:
 #       stock_price : value of the stock
-def get(stock_symbol, redishost):
-#     r = redis.Redis(host='redishost')
+def get(stock_symbol, redisHost):
     stock_price = redisHost.get(stock_symbol)
     if stock_price:
         stock_price = float(stock_price)
