4a1d8b53799cd69dc54f10e5ec9d780ce36d2c49
janhavidudhat <46727626+janhavidudhat@users.noreply.github.com>
2021-03-12 08:32:15 -0800

    Correcting issues from merge

--- worker/src/main.py
+++ worker/src/main.py
@@ -46,7 +46,7 @@ def main():
     redis_cache = redis.Redis(host='redishost')
 
     message_queue = queue.SimpleQueue()
-    command_handler = CMDHandler(response_publisher=publisher, redis_cache)
+    command_handler = CMDHandler(response_publisher=publisher, redis_cache=redis_cache)
 
     t_consumer = Thread(target=queue_thread, args=(message_queue,))
     t_consumer.start()
