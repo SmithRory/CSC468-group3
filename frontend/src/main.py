e7484bae6e4073a9d18146fcb3b83801e9157458
janhavidudhat <janhavidudhat@uvic.ca>
2021-03-21 14:29:25 -0700

    send data to rabbit

--- frontend/src/main.py
+++ frontend/src/main.py
@@ -4,6 +4,12 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, DecimalField, SelectField
 from wtforms.validators import DataRequired, Length
 
+import temp
+import uuid
+import queue
+import threading
+import time
+
 flask_app = Flask(__name__)
 
 # Flask-WTF requires an encryption key
@@ -11,12 +17,12 @@ flask_app.config['SECRET_KEY'] = 'thisisarandomstringofcharacters'
 Bootstrap(flask_app)
 
 class Form(FlaskForm):
-    command = SelectField('Choose an action you want to perform', id="userCommand", validators=[DataRequired()], choices=["ADD", "QUOTE", "BUY", "COMMIT_BUY", "CANCEL_BUY", "SELL", "COMMIT_SELL", "CANCEL_SELL", "SET_BUY_AMOUNT", "CANCEL_SET_BUY", "SET_BUY_TRIGGER", "SET_SELL_AMOUNT", "SET_SELL_TRIGGER", "CANCEL_SET_SELL", "DISPLAY_SUMMARY"])
+    command = SelectField('Choose an action you want to perform', id="userCommand", validators=[DataRequired()],
+        choices=["BUY", "ADD", "QUOTE", "COMMIT_BUY", "CANCEL_BUY", "SELL", "COMMIT_SELL", "CANCEL_SELL", "SET_BUY_AMOUNT", "CANCEL_SET_BUY", "SET_BUY_TRIGGER", "SET_SELL_AMOUNT", "SET_SELL_TRIGGER", "CANCEL_SET_SELL", "DISPLAY_SUMMARY"])
     userId = StringField('What is your user id?', id="user_id", validators=[DataRequired()])
     stockSymbol = StringField('What stock do you want to trade?', id="stock_symbol", validators=[Length(min=1, max=3)])
     amount = DecimalField('Enter the amount of money you want to trade for (Please include cents)', id="funds", validators=[])
     submit = SubmitField('Submit')
-
 
 
 @flask_app.route("/", methods = ['POST', 'GET'])
@@ -31,8 +37,6 @@ def homepage(message=None):
         amount=form.amount.data
         print(command, user_id, stock_symbol, amount)
 
-    # if not formatted right, throw error
-
         return redirect( url_for('api', command=command, user_id=user_id, stock_symbol=stock_symbol, amount=amount) )
 
     else:
@@ -45,19 +49,29 @@ def homepage(message=None):
 @flask_app.route("/api/<command>/<user_id>/<amount>")
 @flask_app.route("/api/<command>/<user_id>/<stock_symbol>/<amount>")
 def api(command, user_id, stock_symbol=None, amount=None):
-    requested_command = "[1] " + command + "," + user_id
+    transactionNum = str(uuid.uuid1())
+    requested_command = "[" + transactionNum + "] " + command + "," + user_id
     if stock_symbol:
         requested_command += "," + stock_symbol
     if amount:
         requested_command += "," + amount
 
+    print(requested_command)
+
+    consume_queue = queue.Queue()
+    publish_queue = queue.Queue()
+
+    threading.Thread(target=temp.consumer_thread, args=(consume_queue,)).start()
+    threading.Thread(target=temp.publisher_thread, args=(publish_queue,), daemon=True).start()
+
     # TO DO
     # send requested_command to rabbitmq
+    consume_queue.put(requested_command)
 
     # TO DO
     # receive confirmation from rabbitmq and store the confirmation text in message
-
-    message = requested_command # this message rn just displays the command
+    message = publish_queue.get()
+#     message = requested_command # this message rn just displays the command
 
     # pass confirmation to form page and display the form page again
     return redirect( url_for('homepage', message=message))
@@ -65,7 +79,7 @@ def api(command, user_id, stock_symbol=None, amount=None):
 
 @flask_app.route("/stockSymbol/<command>")
 def stockSymbolNeeded(command):
-    commands = ["COMMIT_BUY", "CANCEL_BUY", "COMMIT_SELL", "CANCEL_SELL", "DISPLAY_SUMMARY"]
+    commands = ["COMMIT_BUY", "CANCEL_BUY", "COMMIT_SELL", "CANCEL_SELL", "DISPLAY_SUMMARY", "ADD"]
 
     if command in commands:
         return jsonify({'need' : True})
