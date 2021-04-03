88edea5127a2defe67951855844dc5595ccd6806
Oliver Lewis <oliverlewis1339@gmail.com>
2021-04-03 15:13:42 -0700

    Added user login and logout functionallity, organized commands across multiple pages, refactored sending commands to the backend, created UI.

--- frontend/src/main.py
+++ frontend/src/main.py
@@ -1,184 +1,175 @@
-from flask import Flask, render_template, redirect, url_for, request, jsonify
+from flask import Flask, render_template, redirect, url_for, request, jsonify, flash, session
 from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, DecimalField, SelectField
-from wtforms.validators import DataRequired, Length, Optional
+from flask_login import UserMixin, LoginManager, login_user, login_required, current_user, logout_user
+from forms import LoginForm, AddForm, BuySellForm, AutoTransactionForm
+from command_interface import send_command
 
-import rabbit_threads
-import uuid
-import queue
-import threading
-import time
-import os
-import pika
-
-flask_app = Flask(__name__)
+import json
 
 # *** CONFIGURATION start *******************************************
+flask_app = Flask(__name__)
+flask_app.secret_key = b'5f&e@rid5?c1>bab'
 
 # Flask-WTF requires an encryption key
 flask_app.config['SECRET_KEY'] = 'thisisarandomstringofcharacters'
 Bootstrap(flask_app)
 
-# RabbitMQ Queues
-# consume_queue = queue.Queue() 
-# publish_queue = queue.Queue()  
-# threading.Thread(target=rabbit_threads.consumer_thread, args=(consume_queue,), daemon=True).start()
-# threading.Thread(target=rabbit_threads.publisher_thread, args=(publish_queue,), daemon=True).start()
+# Flask-Login
+login_manager = LoginManager()
+login_manager.init_app(flask_app)
+login_manager.login_view = 'login'
 
 # Transaction Number
 transaction_num = 1  # Global
-confirms_recv = 0
-
 # *** CONFIGURATION end *********************************************
 
+# *** USER MODEL start **********************************************
+my_users = {}
 
-class Form(FlaskForm):
-    command = SelectField('Choose an action you want to perform', id="userCommand", validators=[DataRequired()],
-                          choices=["BUY", "ADD", "QUOTE", "COMMIT_BUY", "CANCEL_BUY", "SELL", "COMMIT_SELL",
-                                   "CANCEL_SELL", "SET_BUY_AMOUNT", "CANCEL_SET_BUY", "SET_BUY_TRIGGER",
-                                   "SET_SELL_AMOUNT", "SET_SELL_TRIGGER", "CANCEL_SET_SELL", "DISPLAY_SUMMARY"])
-    userId = StringField('What is your user id?', id="user_id", validators=[DataRequired()])
-    stockSymbol = StringField('What stock do you want to trade?', id="stock_symbol",
-                              validators=[Optional(), Length(min=1, max=3)])
-    amount = DecimalField('Enter the amount of money you want to trade for (Please include cents)', id="funds",
-                          validators=[Optional()])
-    submit = SubmitField('Submit')
+class User(UserMixin):
+    """UserMixin provides the default implementation of the User class
+    that is needed for user sessions."""
+
+    def __init__(self, id):
+        global transaction_num
+        self.id = id
+        # Add user to the db
+        command = f"[{transaction_num}] ADD,{self.id},0.00"
+        send_command(command)
+# *** USER MODEL end ************************************************
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return my_users.get(user_id)
+
+
+@flask_app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        global my_users
+
+        # Login and validate the user.
+        user_id = form.username.data
+        user = User(id=user_id)
+        login_user(user)
+        my_users.update({user_id: user})
+
+        return redirect(url_for('homepage'))
+
+    return render_template('login.html', form=form)
+
+
+@flask_app.route('/logout')
+def logout():
+    my_users.pop(current_user.id)
+    logout_user()
+    return redirect(url_for('homepage'))
 
 
 @flask_app.route("/", methods=['POST', 'GET'])
-@flask_app.route("/<message>", methods=['POST', 'GET'])
-def homepage(message=None):
-    form = Form()
-
-    if form.validate_on_submit():
-        command = form.command.data
-        user_id = form.userId.data
-        stock_symbol = form.stockSymbol.data
-        amount = form.amount.data
-
-        print(command, user_id, stock_symbol, amount)
-
-        return redirect(url_for('api', command=command, user_id=user_id, stock_symbol=stock_symbol, amount=amount))
-
-    return render_template('index.html', form=form, message=message)
-
-
-@flask_app.route("/api/<command>/<user_id>/")
-@flask_app.route("/api/<command>/<user_id>/<stock_symbol>")
-@flask_app.route("/api/<command>/<user_id>/<amount>")
-@flask_app.route("/api/<command>/<user_id>/<stock_symbol>/<amount>")
-def api(command, user_id, stock_symbol=None, amount=None):
-    #global consume_queue
-    #global publish_queue
+@login_required
+def homepage():
     global transaction_num
-    global confirms_recv
+    form = AddForm()
 
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host="rabbitmq")
-    )
-    publisher_channel = connection.channel()
-    consume_channel = connection.channel()
+    account_summary = send_command(f"[{transaction_num}] DISPLAY_SUMMARY,{current_user.id}")
+    account_summary = json.loads(account_summary[23:-1])  # Gets just the json data
 
-    publisher_channel.exchange_declare(
-        exchange=os.environ["FRONTEND_EXCHANGE"]
-    )
+    message = None
+    if 'message' in request.args:
+        message = request.args.get('message')
 
-    consume_channel.exchange_declare(
-        exchange=os.environ["CONFIRMS_EXCHANGE"],
-        exchange_type='fanout'
-    )
+    return render_template('index.html', form=form, message=message, account_summary=account_summary)
 
-    result = consume_channel.queue_declare(queue='')
-    queue_name = result.method.queue
-    consume_channel.queue_bind(
-        exchange=os.environ["CONFIRMS_EXCHANGE"],
-        queue=queue_name
-    )
 
-    # consume_queue = queue.Queue() 
-    # publish_queue = queue.Queue()  
-    # t_consume = threading.Thread(target=rabbit_threads.consumer_thread, args=(consume_queue,))
-    # t_publish = threading.Thread(target=rabbit_threads.publisher_thread, args=(publish_queue,))
-    # t_consume.start()
-    # t_publish.start()
+@flask_app.route("/buysell", methods=['POST', 'GET'])
+@login_required
+def buy_sell():
+    global transaction_num
+    form = BuySellForm()
 
+    message = None
+    if 'message' in request.args:
+        message = request.args.get('message')
+
+    return render_template('buysell.html', form=form, message=message)
+
+
+@flask_app.route("/autotransaction", methods=['POST', 'GET'])
+@login_required
+def auto_transactions():
+    global transaction_num
+    form = AutoTransactionForm()
+
+    message = None
+    if 'message' in request.args:
+        message = request.args.get('message')
+
+    return render_template('autotransactions.html', form=form, message=message)
+
+
+@flask_app.route("/add_api", methods=['POST'])
+@login_required
+def add_api():
+    """ Returns the response for the given command.
+
+    The command is given as form
+    """
+    global transaction_num
+
+    # Handle the form data
+    command = "ADD"
+    user_id = current_user.id
     requested_command = f"[{transaction_num}] {command},{user_id}"
     transaction_num += 1
 
-    if stock_symbol:
-        requested_command = f"{requested_command},{stock_symbol}"
+    if request.form['amount'] is not None:
+        requested_command = f"{requested_command},{request.form['amount']}"
 
-    if amount:
-        requested_command = f"{requested_command},{amount}"
+    message = send_command(requested_command)
 
-    publisher_channel.basic_publish(
-        exchange=os.environ["FRONTEND_EXCHANGE"],
-        routing_key="frontend",
-        body=requested_command,
-        properties=pika.BasicProperties(),
-        mandatory=True
-    )
-
-    print(f"Successfully sent command to publish queue: {requested_command}.")
-
-    # receive confirmation from rabbitmq and store the confirmation text in message
-    print("Waiting to get response from consume queue...")
-
-    message = None
-    while(message is None):
-        method_frame, header_frame, message = consume_channel.basic_get(queue=queue_name)
-
-    message = message.decode('utf-8')
-
-    confirms_recv += 1
-    print(f"Total confirms received: {confirms_recv}")
-#     message = "Sentttt"
-    print(f"Received response from consume queue:"
-          f"\n\tOriginal Command: {requested_command}"
-          f"\n\tReceived Message: {message}\n")
-
-    #     message = requested_command # this message rn just displays the command
-
-    # pass confirmation to form page and display the form page again
-    x = url_for('homepage', message=message, _external=True)
-    print(f"url is {x}")
-
-    # t_publish.join()
-    # t_consume.join()
-    connection.close()
-
-    return redirect(x)
+    return redirect(url_for('homepage', message=message))
 
 
-'''
-Create publisher and consumer in their own thread. They each have a connection
-to rabbitmq only because sharing one between them would require a large rewright
-of their code.
+@flask_app.route("/buysell_api", methods=['POST'])
+@login_required
+def buysell_api():
+    global transaction_num
 
-consume_queue = queue.Queue()
-publish_queue = queue.Queue()
+    # Handle the form data.
+    user_id = current_user.id
+    requested_command = f"[{transaction_num}] {request.form['command']},{user_id}"
+    transaction_num += 1
 
-consumer = Consumer(
-    queue=consume_queue,
-    connection_param="rabbitmq",
-    exchange_name=os.environ["CONFIRMS_EXCHANGE"],
-    queue_name="frontend",
-    routing_key="frontend"
-)
+    if request.form['stockSymbol'] is not None:
+        requested_command = f"{requested_command},{request.form['stockSymbol']}"
 
-self.publisher = Publisher(
-    connection_param=self._send_address,
-    exchange_name=os.environ["FRONTEND_EXCHANGE"],
-    queue = publish_queue
-)
+        if request.form['amount'] is not None:
+            requested_command = f"{requested_command},{request.form['amount']}"
 
-Then in each thread call .run()
-This is a blocking call
+    message = send_command(requested_command)
 
-you can then get or put using:
-consume_queue.put(data) and publish_queue.get() from the main thread
+    return redirect(url_for('buy_sell', message=message))
 
-Note that both of these calls are fully thread safe and can block
 
-'''
+@flask_app.route("/auto_api", methods=['POST'])
+@login_required
+def auto_api():
+    global transaction_num
+
+    # Handle the form data.
+    user_id = current_user.id
+    requested_command = f"[{transaction_num}] {request.form['command']},{user_id}"
+    transaction_num += 1
+
+    if request.form['stockSymbol'] is not None:
+        requested_command = f"{requested_command},{request.form['stockSymbol']}"
+
+        if request.form['amount'] is not None:
+            requested_command = f"{requested_command},{request.form['amount']}"
+
+    message = send_command(requested_command)
+
+    return redirect(url_for('auto_transactions', message=message))
